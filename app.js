42be7d0af5d12f48cfd0ed62b5b9919c340a3cda
SimVac <simonevacca932@gmail.com>
2023-11-30 22:02:29 +0100

    Aggiunge dati di test

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const express = require("express");
+const path = require("path");
+const bodyParser = require('body-parser');
+
+const { data } = require("./data.js");
+
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req, res) => {
+    res.render(path.join(__dirname, './templates/index.ejs'));
+});
+
+app.get("/data", (req, res) => {
+    res.send(data);
+})
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Test</title>
-  </head>
-  <body>
-    <h1>Hello World!</h1>
-  </body>
-</html>
-`
