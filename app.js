faf772e2ea34d5e6291fa2ef476649d1ea130de5
gabbalb <balbiani.gabriele@gmail.com>
2023-12-15 09:18:11 +0100

    Added password for admin interface

--- app.js
+++ app.js
@@ -1,16 +1,54 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require('body-parser');
-
 const { data } = require("./data.js");
-
-
+const session = require('express-session');
 const app = express();
 const port = process.env.PORT || 3001;
-
+app.use( express.static("public"))
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: true
+}));
+
+function passwordProtected(req, res, next) {
+    if (req.session.password === 'passw0rd') {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+app.get('/login', (req, res) => {
+    res.send('<form action="/login" method="POST"><input type="password" name="password"><button type="submit">Submit</button></form>');
+});
+
+app.post('/login', (req, res) => {
+    if (req.body.password === 'passw0rd') {
+        req.session.password = req.body.password;
+        res.redirect('/admin');
+    } else {
+        res.send('Incorrect Password! <a href="/login">Try again</a>');
+    }
+});
+
+// Route for the login page
+app.get('/login', (req, res) => {
+    res.send('<form action="/login" method="POST"><input type="password" name="password"><button type="submit">Submit</button></form>');
+});
+
+// Apply the middleware to your EJS route
+app.get('/admin', passwordProtected, (req, res) => {
+    res.render(path.join(__dirname, './templates/admin.ejs'), {
+        "materie": data.materie
+    });
+});
 
 app.get("/", (req, res) => {
     res.render(path.join(__dirname, './templates/index.ejs'), {
@@ -44,12 +82,6 @@ app.get("/data", (req, res) => {
 
 app.get("/login", (req, res) => {
     res.render(path.join(__dirname, './templates/login.ejs'));
-});
-
-app.get("/admin", (req, res) => {
-    res.render(path.join(__dirname, './templates/admin.ejs'), {
-        "materie": data.materie
-    });
 });
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
