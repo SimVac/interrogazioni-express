05cd57cf4522c18e0b62d550cbfa51d501b2a8b7
gabbalb <balbiani.gabriele@gmail.com>
2023-12-18 09:47:43 +0100

    Colpa di Sinisterra Boya

--- data.js
+++ data.js
@@ -1,3 +1,89 @@
-const data = {"materie":{"tpi":{"nome":"Tecnologie e progettazione di sistemi informatici e di telecomunicazioni","abbreviazione":"TPI","endpoint":"tpi","ordine":[{"nome":"Riccardo","cognome":"Poli","interrogato":false},{"nome":"Francesco","cognome":"Mannozzi","interrogato":false},{"nome":"Gianluca","cognome":"Stornati","interrogato":false},{"nome":"Gabriel","cognome":"Sinisterra Boya","interrogato":false},{"nome":"Riccardo","cognome":"Nulli","interrogato":false},{"nome":"Francis Carl","cognome":"Banua","interrogato":false},{"nome":"Meris","cognome":"Muca","interrogato":false},{"nome":"Mattia","cognome":"Corradini","interrogato":false},{"nome":"Paolo","cognome":"Zucca","interrogato":false},{"nome":"Alessandro","cognome":"Alberti","interrogato":false},{"nome":"Sebastiano","cognome":"Pea","interrogato":false},{"nome":"Nicolas","cognome":"Mantelli","interrogato":false},{"nome":"Luca","cognome":"Zanotti","interrogato":false},{"nome":"Simone","cognome":"Vacca","interrogato":false},{"nome":"Simone","cognome":"Orizio","interrogato":false},{"nome":"Alessandro","cognome":"Rago","interrogato":false},{"nome":"Andrea","cognome":"Scarsato","interrogato":false},{"nome":"Gabriele","cognome":"Balbiani","interrogato":false}]},"storia":{"nome":"Storia","abbreviazione":"Storia","endpoint":"storia","ordine":[{"nome":"Alessandro","cognome":"Alberti","interrogato":false},{"nome":"Gabriele","cognome":"Balbiani","interrogato":false},{"nome":"Francis Carl","cognome":"Banua","interrogato":false},{"nome":"Mattia","cognome":"Corradini","interrogato":false},{"nome":"Francesco","cognome":"Mannozzi","interrogato":false},{"nome":"Nicolas","cognome":"Mantelli","interrogato":false},{"nome":"Meris","cognome":"Muca","interrogato":false},{"nome":"Riccardo","cognome":"Nulli","interrogato":false},{"nome":"Simone","cognome":"Orizio","interrogato":false},{"nome":"Sebastiano","cognome":"Pea","interrogato":false},{"nome":"Riccardo","cognome":"Poli","interrogato":false},{"nome":"Alessandro","cognome":"Rago","interrogato":false},{"nome":"Andrea","cognome":"Scarsato","interrogato":false},{"nome":"Gabriel","cognome":"Sinisterra Boya","interrogato":false},{"nome":"Gianluca","cognome":"Stornati","interrogato":false},{"nome":"Simone","cognome":"Vacca","interrogato":false},{"nome":"Luca","cognome":"Zanotti","interrogato":false},{"nome":"Paolo","cognome":"Zucca","interrogato":false}]}}}
+const data = {
+    "materie": {
+        "tpi": {
+            "nome": "Tecnologie e progettazione di sistemi informatici e di telecomunicazioni",
+            "abbreviazione": "TPI",
+            "endpoint": "tpi",
+            "ordine": [{"nome": "Riccardo", "cognome": "Poli", "interrogato": false}, {
+                "nome": "Francesco",
+                "cognome": "Mannozzi",
+                "interrogato": false
+            }, {"nome": "Gianluca", "cognome": "Stornati", "interrogato": false}, {
+                "nome": "Gabriel",
+                "cognome": "Sinisterra",
+                "interrogato": false
+            }, {"nome": "Riccardo", "cognome": "Nulli", "interrogato": false}, {
+                "nome": "Francis Carl",
+                "cognome": "Banua",
+                "interrogato": false
+            }, {"nome": "Meris", "cognome": "Muca", "interrogato": false}, {
+                "nome": "Mattia",
+                "cognome": "Corradini",
+                "interrogato": false
+            }, {"nome": "Paolo", "cognome": "Zucca", "interrogato": false}, {
+                "nome": "Alessandro",
+                "cognome": "Alberti",
+                "interrogato": false
+            }, {"nome": "Sebastiano", "cognome": "Pea", "interrogato": false}, {
+                "nome": "Nicolas",
+                "cognome": "Mantelli",
+                "interrogato": false
+            }, {"nome": "Luca", "cognome": "Zanotti", "interrogato": false}, {
+                "nome": "Simone",
+                "cognome": "Vacca",
+                "interrogato": false
+            }, {"nome": "Simone", "cognome": "Orizio", "interrogato": false}, {
+                "nome": "Alessandro",
+                "cognome": "Rago",
+                "interrogato": false
+            }, {"nome": "Andrea", "cognome": "Scarsato", "interrogato": false}, {
+                "nome": "Gabriele",
+                "cognome": "Balbiani",
+                "interrogato": false
+            }]
+        }, "storia": {
+            "nome": "Storia",
+            "abbreviazione": "Storia",
+            "endpoint": "storia",
+            "ordine": [{"nome": "Alessandro", "cognome": "Alberti", "interrogato": false}, {
+                "nome": "Gabriele",
+                "cognome": "Balbiani",
+                "interrogato": false
+            }, {"nome": "Francis Carl", "cognome": "Banua", "interrogato": false}, {
+                "nome": "Mattia",
+                "cognome": "Corradini",
+                "interrogato": false
+            }, {"nome": "Francesco", "cognome": "Mannozzi", "interrogato": false}, {
+                "nome": "Nicolas",
+                "cognome": "Mantelli",
+                "interrogato": false
+            }, {"nome": "Meris", "cognome": "Muca", "interrogato": false}, {
+                "nome": "Riccardo",
+                "cognome": "Nulli",
+                "interrogato": false
+            }, {"nome": "Simone", "cognome": "Orizio", "interrogato": false}, {
+                "nome": "Sebastiano",
+                "cognome": "Pea",
+                "interrogato": false
+            }, {"nome": "Riccardo", "cognome": "Poli", "interrogato": false}, {
+                "nome": "Alessandro",
+                "cognome": "Rago",
+                "interrogato": false
+            }, {"nome": "Andrea", "cognome": "Scarsato", "interrogato": false}, {
+                "nome": "Gabriel",
+                "cognome": "Sinisterra",
+                "interrogato": false
+            }, {"nome": "Gianluca", "cognome": "Stornati", "interrogato": false}, {
+                "nome": "Simone",
+                "cognome": "Vacca",
+                "interrogato": false
+            }, {"nome": "Luca", "cognome": "Zanotti", "interrogato": false}, {
+                "nome": "Paolo",
+                "cognome": "Zucca",
+                "interrogato": false
+            }]
+        }
+    }
+}
 
-module.exports = { data };
+module.exports = {data};
