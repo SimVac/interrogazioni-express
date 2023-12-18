970a6bfc52690976938e536d55edc75bd3f85773
SimVac <103424187+SimVac@users.noreply.github.com>
2023-12-18 03:04:23 -0500

    Aggiornamento Dati

--- data.js
+++ data.js
@@ -1,3 +1,3 @@
-const data = {"materie":{"tpi":{"nome":"Tecnologie e progettazione di sistemi informatici e di telecomunicazioni","abbreviazione":"TPI","endpoint":"tpi","ordine":[{"nome":"Simone","cognome":"Vacca","interrogato":true},{"nome":"Gabriele","cognome":"Balbiani","interrogato":false}]},"storia":{"nome":"Storia","abbreviazione":"Storia","endpoint":"storia","ordine":[{"nome":"Simone","cognome":"Vacca","interrogato":false}]}}}
+const data = {"materie":{"tpi":{"nome":"Tecnologie e progettazione di sistemi informatici e di telecomunicazioni","abbreviazione":"TPI","endpoint":"tpi","ordine":[{"nome":"Simone","cognome":"Vacca","interrogato":true},{"nome":"Gabriele","cognome":"Balbiani","interrogato":true}]},"storia":{"nome":"Storia","abbreviazione":"Storia","endpoint":"storia","ordine":[{"nome":"Simone","cognome":"Vacca","interrogato":false}]}}}
 
 module.exports = { data };
